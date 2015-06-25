7004070040d08b1046c278597c66a9961f8588a9
Michael Houghton <michael@cakecoded.com>
2015-06-25 16:44:26 +0100

    CK Editor helper

---
+++ View/Helper/CkHelper.php
@@ -0,0 +1,52 @@
+<?php
+class CkHelper extends AppHelper {
+
+    public $helpers = array('Html', 'Form');
+
+/**
+* Extention of the Form Helper to insert CK Editor for a form input
+*
+* @param string $input The name of the field, can be field_name or Model.field_name
+* @param array $options Options include $options['label'] for a custom label - this can be expanded on if required
+*/
+    public function input($input, $options = array()) {
+        if (!empty($options['div'])) {
+            echo __('<div class="%s">', $options['div']);
+        }
+
+        echo $this->Html->script('//cdn.ckeditor.com/4.4.5.1/basic/ckeditor.js');
+
+        $input = explode('.', $input);
+        if (empty($input[1])) {
+        	$field = $input[0];
+        	$model = $this->Form->model();
+        } else {
+        	$model = $input[0];
+        	$field = $input[1];
+        }
+
+        if (!empty($options['label'])) {
+        	echo '<h2>'.$options['label'].'</h2>';
+        } else {
+        	echo '<h2>'.Inflector::humanize(Inflector::underscore($field)).'</h2>';
+        }
+
+        if (!empty($options['value'])) {
+            $this->request->data[$model][$field] = $options['value'];
+        }
+
+        echo $this->Form->error($model.'.'.$field);
+        echo $this->Form->input($model.'.'.$field, array('type' => 'textarea', 'label' => false, 'error' => false, 'required' => false, 'div' => false));
+		?>
+			<script type="text/javascript">
+				CKEDITOR.replace('<?php echo Inflector::camelize($model.'_'.$field); ?>');
+			</script>
+
+			<p>&nbsp;</p>
+		<?php
+
+        if (!empty($options['div'])) {
+            echo '</div>';
+        }
+    }
+}
